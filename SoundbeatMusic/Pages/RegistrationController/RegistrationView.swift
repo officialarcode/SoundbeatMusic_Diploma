fb5033538a2c7ff8f327fe910b7996efdc313698
officialarcode <officialarcodemusic06@gmail.com>
2026-01-14 17:38:05 +0500

    Реализованный дизайн страницы Регистрации

---
+++ SoundbeatMusic/Pages/RegistrationController/RegistrationView.swift
@@ -0,0 +1,18 @@
+//
+//  RegistrationView.swift
+//  SoundbeatMusic
+//
+//  Created by Арсений Морозов on 14.01.2026.
+//
+
+import SwiftUI
+
+struct RegistrationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    RegistrationView()
+}
